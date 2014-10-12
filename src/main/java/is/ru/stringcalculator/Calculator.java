a9c3034f34b8b7854a18696c6a6ef23da5da23e4
sindrikaldal <sindrimars@gmail.com>
2014-10-12 22:39:04 +0000

    Added a test for string with newline

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -9,6 +9,9 @@ public class Calculator {
 		else if(text.contains(",")){
 			return sum(splitNumbers(text));
 		}
+		else if(text.contains("\n")) {
+			return sum(splitNumbers(text));
+		}
 		else
 			return 1;
 	}
@@ -18,7 +21,8 @@ public class Calculator {
 	}
 
 	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
+	    return numbers.split("\n|\\,");
+
 	}
       
     private static int sum(String[] numbers){
@@ -28,6 +32,8 @@ public class Calculator {
 		}
 		return total;
     }
+
+
 
 
 
